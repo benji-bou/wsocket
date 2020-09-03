dcd296ef4703d64ef6f09fdcaf3e9b1717ca944f
BBO <benjamin.bouachour@bioserenity.com>
2020-09-03 09:32:49 +0200

    update type of wsocket meeage

--- wsocket.go
+++ wsocket.go
@@ -1,16 +1,10 @@
 package wsocket
 
 import (
-	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
-)
-
-var (
-	ErrSignalChannelClosed = errors.New("client signal is closed")
 )
 
 var (
@@ -19,13 +13,11 @@ var (
 	}
 )
 
-type Communicator interface {
-	SendMessage(message interface{}) error
-}
-
 type Socket struct {
-	write      chan interface{}
-	read       chan []byte
+	twrite     chan []byte
+	tread      chan []byte
+	bwrite     chan []byte
+	bread      chan []byte
 	errc       chan error
 	closeState bool
 
@@ -38,54 +30,83 @@ func (c *Socket) Close() {
 	c.closeState = true
 }
 
-func (c *Socket) GetRead() <-chan []byte {
-	return c.read
+func (c *Socket) Read(messagType int) <-chan []byte {
+	switch messagType {
+	case websocket.BinaryMessage:
+		return c.bread
+	case websocket.TextMessage:
+		return c.tread
+	}
+	return c.tread
 }
 
-func (c *Socket) GetError() <-chan error {
+func (c *Socket) Error() <-chan error {
 	return c.errc
 }
 
-func (c *Socket) GetWrite() chan<- interface{} {
-	return c.write
-}
-
-func (c *Socket) SendMessage(message interface{}) {
-	if c.closeState == false {
-		c.GetWrite() <- message
-
+func (c *Socket) Write(messagType int) chan<- []byte {
+	switch messagType {
+	case websocket.BinaryMessage:
+		return c.bwrite
+	case websocket.TextMessage:
+		return c.twrite
 	}
+	return c.twrite
 }
 
-func concurrentRead(cl *Socket) {
+func (c *Socket) concurrentRead() {
 	for {
-		// event := reflect.New(cl.incomingType).Interface()
-		event := json.RawMessage{}
-		err := cl.co.ReadJSON(&event)
+
+		t, b, err := c.co.ReadMessage()
 		if err != nil {
-			cl.Close()
-			cl.errc <- err
+			select {
+			case c.errc <- err:
+			default:
+				log.Println(err)
+			}
 			return
-		} else {
-			// log.Println("Socket received data", string(event))
-			cl.read <- event
+		}
+		switch t {
+		case websocket.BinaryMessage:
+			select {
+			case c.bread <- b:
+			default:
+			}
+		case websocket.TextMessage:
+			select {
+			case c.tread <- b:
+			default:
+			}
 		}
 	}
 }
 
-func concurentWrite(cl *Socket) {
+func (c *Socket) concurentWrite() {
 	for {
 		select {
-		case json, ok := <-cl.write:
-			if ok == true {
-				if err := cl.co.WriteJSON(json); err != nil {
-					if cl.closeState == false {
-						cl.errc <- err
-						return
+		case b := <-c.bwrite:
+
+			if err := c.co.WriteMessage(websocket.BinaryMessage, b); err != nil {
+				if c.closeState == false {
+					select {
+					case c.errc <- err:
+					default:
+						log.Println("unable to send bwrite error to channel", err)
 					}
+					return
 				}
-			} else {
-				return
+
+			}
+		case t := <-c.twrite:
+			if err := c.co.WriteMessage(websocket.TextMessage, t); err != nil {
+				if c.closeState == false {
+					select {
+					case c.errc <- err:
+					default:
+						log.Println("unable to send twrite error to channel", err)
+					}
+					return
+				}
 			}
 
 		}
@@ -101,9 +122,9 @@ func ConnectSocket(addr string) (*Socket, error) {
 	if err != nil {
 		return nil, err
 	}
-	cl := &Socket{co: conn, write: make(chan interface{}), read: make(chan []byte), errc: make(chan error), closeState: false}
-	go concurentWrite(cl)
-	go concurrentRead(cl)
+	cl := &Socket{co: conn, twrite: make(chan []byte, 256), tread: make(chan []byte, 256), bwrite: make(chan []byte, 256), bread: make(chan []byte, 256), errc: make(chan error), closeState: false}
+	go cl.concurentWrite()
+	go cl.concurrentRead()
 	return cl, nil
 }
 
@@ -113,9 +134,9 @@ func AcceptNewSocket(w http.ResponseWriter, r *http.Request) (*Socket, error) {
 		return nil, err
 	}
 
-	cl := &Socket{co: conn, write: make(chan interface{}), read: make(chan []byte), errc: make(chan error), closeState: false}
-	go concurentWrite(cl)
-	go concurrentRead(cl)
+	cl := &Socket{co: conn, twrite: make(chan []byte, 256), tread: make(chan []byte, 256), bwrite: make(chan []byte, 256), bread: make(chan []byte, 256), errc: make(chan error), closeState: false}
+	go cl.concurentWrite()
+	go cl.concurrentRead()
 	return cl, nil
 }
 
